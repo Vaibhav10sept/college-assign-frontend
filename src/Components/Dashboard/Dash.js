7ba69ba9072c207ccfcfce39fe7ef79c4c93f0d5
Vaibhav10sept <vaibhavtiwari10sept@gmail.com>
2021-04-29 18:13:19 +0530

    bfbb

--- src/Components/Dashboard/Dash.js
+++ src/Components/Dashboard/Dash.js
@@ -28,15 +28,15 @@ function Dash() {
         <input type="checkbox" id="mobilmenu" />
         <div class="sidebar">
           <Link to="/">
-          <div class="logo"><i class="fa fa-shopping-bag"></i><h2>Welcome User</h2></div></Link>
+            <div class="logo"><i class="fa fa-shopping-bag"></i><h2>Welcome User</h2></div></Link>
           <div class="menu">
             <Slide top cascade>
               <ul>
 
-                <NavLink activeStyle={{color: 'red!important'}} to="/state" className="Nav_link" > <li class="rout"> <i class="fa fa-home"></i>  State</li></NavLink>
+                <NavLink activeStyle={{ color: 'red!important' }} to="/state" className="Nav_link" > <li class="rout"> <i class="fa fa-home"></i>  State</li></NavLink>
 
-                <NavLink activeStyle={{color: 'red!important'}} to="/course" className="Nav_link" >  <li class="rout">  <i class="fa fa-home"></i>    Courses</li></NavLink>
-                <NavLink activeStyle={{color: 'red!important'}} to="/chart" className="Nav_link" >     <li class="rout">  <i class="fa fa-home"></i>     Charts</li></NavLink>
+                <NavLink activeStyle={{ color: 'red!important' }} to="/course" className="Nav_link" >  <li class="rout">  <i class="fa fa-home"></i>    Courses</li></NavLink>
+                <NavLink activeStyle={{ color: 'red!important' }} to="/chart" className="Nav_link" >     <li class="rout">  <i class="fa fa-home"></i>     Charts</li></NavLink>
                 <NavLink to="/state" className="Nav_link" > <li class="rout"> <i class="fa fa-home"></i>   State</li></NavLink>
 
                 <NavLink to="/course" className="Nav_link" >  <li class="rout">  <i class="fa fa-home"></i>    Courses</li></NavLink>
@@ -61,25 +61,25 @@ function Dash() {
               <i class="fa fa-search"></i>
             </div>
             <div class="kullanıcı">
-              <i class="fas fa-wallet"></i>
-              <i class="fa fa-envelope" aria-hidden="true"></i>
-              <label style={{ position: "relative" }} >
-                <i id="bildirim" class="fa fa-bell" aria-hidden="true"></i>
-              </label>
+              <a target="_blank" href="https://portfolio-vaibhav-tiwari.herokuapp.com"><i class="fas fa-wallet"></i></a>
+              <a target="_blank" href="https://github.com/Vaibhav10sept"><i class="fa fa-github" aria-hidden="true"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/vaibhav-tiwari-8043b6175/"><label style={{ position: "relative" }} >
+                <i id="bildirim" class="fa fa-linkedin" aria-hidden="true"></i>
+              </label></a>
               <span>Vaibhav Tiwari</span>
               <i class="fas fa-chevron-down"></i>
 
             </div>
           </div>
           <Switch>
-          <Route exact path="/" component={Landing} />
+            <Route exact path="/" component={Landing} />
 
             <Route exact path="/state" component={CollegeDash} />
             <Route exact path="/course" component={StudentDash} />
             <Route exact path="/chart" component={Graph} />
             <Route exact path="/college/:id" component={Institute} />
 
-           
+
           </Switch>
           {/* <College />
         <Graph />
