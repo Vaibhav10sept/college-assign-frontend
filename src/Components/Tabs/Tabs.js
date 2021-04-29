add80eda836d7376664d02c2fce3489aff423bc2
SansakrSachan <Sanskar841signh@gmail.com>
2021-04-29 20:45:55 +0530

    ahsda

--- src/Components/Tabs/Tabs.js
+++ src/Components/Tabs/Tabs.js
@@ -13,9 +13,9 @@ const [showcollege, setshowcollege] = useState(false)
     <div className="tab_cont">
       <section class="tabs">
         <input id="tab-1" onClick={()=>{setshowstudent(true);setshowcollege(false)}} type="radio" name="radio-set" class="tab-selector-1" />
-        <label for="tab-1"  class="tab-label-1">Student List</label>
+        <label for="tab-1"  class="tab-label-1">View Student List</label>
         <input id="tab-2" onClick={()=>{setshowstudent(false);setshowcollege(true)}} type="radio" name="radio-set" class="tab-selector-2" />
-        <label for="tab-2" class="tab-label-2">Similar Colleges</label>
+        <label for="tab-2" class="tab-label-2">View Similar Colleges</label>
 {showstudent?<Student collegeId={collegeId} />:null}
 {showcollege?<College collegeId={collegeId} />:null}
 
