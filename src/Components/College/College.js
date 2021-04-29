3144c901e77b8141ad280aabc0164ec7b8840c21
SansakrSachan <Sanskar841signh@gmail.com>
2021-04-29 12:51:23 +0530

    college

--- src/Components/College/College.js
+++ src/Components/College/College.js
@@ -4,178 +4,251 @@ import Fade from 'react-reveal/Fade';
 function College() {
   return (
     <div className="College_container">
-      <div class="wrapper">
+      <div class="College_wrapper">
         <Fade left cascade>
-          <div class="table">
+          <div class="College_table">
 
-            <div class="row header">
-              <div class="cell">
+            <div class="College_row College_header">
+              <div class="College_cell">
                 ID
       </div>
-              <div class="cell">
+              <div class="College_cell">
                 Name
       </div>
-              <div class="cell">
+              <div class="College_cell">
                 Year Founded
       </div>
-              <div class="cell">
+              <div class="College_cell">
                 City
       </div>
-              <div class="cell">
+              <div class="College_cell">
                 State
       </div>
-              <div class="cell">
+              <div class="College_cell">
                 Country
       </div>
               
-              <div class="cell">
+              <div class="College_cell">
                 Courses
       </div>
             </div>
-            <div class="row">
-              <div class="cell" data-title="Name">
-              100100101
+            <div class="College_row College_header">
+              <div class="College_cell">
+                ID
       </div>
-              <div class="cell" data-title="Age">
-                College1
+              <div class="College_cell">
+                Name
       </div>
-              <div class="cell" data-title="Occupation">
-                2009
+              <div class="College_cell">
+                Year Founded
       </div>
-              <div class="cell" data-title="Location">
-                Kanpur
+              <div class="College_cell">
+                City
       </div>
-              <div class="cell" data-title="Location">
-                Uttar Pradesh
+              <div class="College_cell">
+                State
       </div>
-              <div class="cell" data-title="Location">
-                India
+              <div class="College_cell">
+                Country
       </div>
-
-              <div class="cell" data-title="Location">
-                CS,ECE,IT,MECH
-      </div>
-            </div>
-            <div class="row">
-              <div class="cell" data-title="Name">
-                100100101
-      </div>
-              <div class="cell" data-title="Age">
-              College1
-      </div>
-              <div class="cell" data-title="Occupation">
-                2009
-      </div>
-              <div class="cell" data-title="Location">
-                Kanpur
-      </div>
-              <div class="cell" data-title="Location">
-                Uttar Pradesh
-      </div>
-              <div class="cell" data-title="Location">
-                India
-      </div>
-
-              <div class="cell" data-title="Location">
-                CS,ECE,IT,MECH
+              
+              <div class="College_cell">
+                Courses
       </div>
             </div>
-            <div class="row">
-              <div class="cell" data-title="Name">
-              100100101
+            <div class="College_row College_header">
+              <div class="College_cell">
+                ID
       </div>
-              <div class="cell" data-title="Age">
-              College1
+              <div class="College_cell">
+                Name
       </div>
-              <div class="cell" data-title="Occupation">
-                2009
+              <div class="College_cell">
+                Year Founded
       </div>
-              <div class="cell" data-title="Location">
-                Kanpur
+              <div class="College_cell">
+                City
       </div>
-              <div class="cell" data-title="Location">
-                Uttar Pradesh
+              <div class="College_cell">
+                State
       </div>
-              <div class="cell" data-title="Location">
-                India
+              <div class="College_cell">
+                Country
       </div>
-
-              <div class="cell" data-title="Location">
-                CS,ECE,IT,MECH
-      </div>
-            </div>
-            <div class="row">
-              <div class="cell" data-title="Name">
-              100100101
-      </div>
-              <div class="cell" data-title="Age">
-              College1
-      </div>
-              <div class="cell" data-title="Occupation">
-                2009
-      </div>
-              <div class="cell" data-title="Location">
-                Kanpur
-      </div>
-              <div class="cell" data-title="Location">
-                Uttar Pradesh
-      </div>
-              <div class="cell" data-title="Location">
-                India
-      </div>
-              <div class="cell" data-title="Location">
-                CS,ECE,IT,MECH
+              
+              <div class="College_cell">
+                Courses
       </div>
             </div>
-            <div class="row">
-              <div class="cell" data-title="Name">
-              100100101
+            <div class="College_row College_header">
+              <div class="College_cell">
+                ID
       </div>
-              <div class="cell" data-title="Age">
-              College1
+              <div class="College_cell">
+                Name
       </div>
-              <div class="cell" data-title="Occupation">
-                2009
+              <div class="College_cell">
+                Year Founded
       </div>
-              <div class="cell" data-title="Location">
-                Kanpur
+              <div class="College_cell">
+                City
       </div>
-              <div class="cell" data-title="Location">
-                Uttar Pradesh
+              <div class="College_cell">
+                State
       </div>
-              <div class="cell" data-title="Location">
-                India
+              <div class="College_cell">
+                Country
       </div>
-              <div class="cell" data-title="Location">
-                CS,ECE,IT,MECH
+              
+              <div class="College_cell">
+                Courses
       </div>
             </div>
-            <div class="row">
-              <div class="cell" data-title="Name">
-              100100101
+            <div class="College_row College_header">
+              <div class="College_cell">
+                ID
       </div>
-              <div class="cell" data-title="Age">
-              College1
+              <div class="College_cell">
+                Name
       </div>
-              <div class="cell" data-title="Occupation">
-                2009
+              <div class="College_cell">
+                Year Founded
       </div>
-              <div class="cell" data-title="Location">
-                Kanpur
+              <div class="College_cell">
+                City
       </div>
-              <div class="cell" data-title="Location">
-                Uttar Pradesh
+              <div class="College_cell">
+                State
       </div>
-              <div class="cell" data-title="Location">
-                India
+              <div class="College_cell">
+                Country
       </div>
-
-              <div class="cell" data-title="Location">
-                CS,ECE,IT,MECH
+              
+              <div class="College_cell">
+                Courses
+      </div>
+            </div>
+            <div class="College_row College_header">
+              <div class="College_cell">
+                ID
+      </div>
+              <div class="College_cell">
+                Name
+      </div>
+              <div class="College_cell">
+                Year Founded
+      </div>
+              <div class="College_cell">
+                City
+      </div>
+              <div class="College_cell">
+                State
+      </div>
+              <div class="College_cell">
+                Country
+      </div>
+              
+              <div class="College_cell">
+                Courses
+      </div>
+            </div>
+            <div class="College_row College_header">
+              <div class="College_cell">
+                ID
+      </div>
+              <div class="College_cell">
+                Name
+      </div>
+              <div class="College_cell">
+                Year Founded
+      </div>
+              <div class="College_cell">
+                City
+      </div>
+              <div class="College_cell">
+                State
+      </div>
+              <div class="College_cell">
+                Country
+      </div>
+              
+              <div class="College_cell">
+                Courses
+      </div>
+            </div>
+            <div class="College_row College_header">
+              <div class="College_cell">
+                ID
+      </div>
+              <div class="College_cell">
+                Name
+      </div>
+              <div class="College_cell">
+                Year Founded
+      </div>
+              <div class="College_cell">
+                City
+      </div>
+              <div class="College_cell">
+                State
+      </div>
+              <div class="College_cell">
+                Country
+      </div>
+              
+              <div class="College_cell">
+                Courses
+      </div>
+            </div>
+            <div class="College_row College_header">
+              <div class="College_cell">
+                ID
+      </div>
+              <div class="College_cell">
+                Name
+      </div>
+              <div class="College_cell">
+                Year Founded
+      </div>
+              <div class="College_cell">
+                City
+      </div>
+              <div class="College_cell">
+                State
+      </div>
+              <div class="College_cell">
+                Country
+      </div>
+              
+              <div class="College_cell">
+                Courses
+      </div>
+            </div>
+            <div class="College_row College_header">
+              <div class="College_cell">
+                ID
+      </div>
+              <div class="College_cell">
+                Name
+      </div>
+              <div class="College_cell">
+                Year Founded
+      </div>
+              <div class="College_cell">
+                City
+      </div>
+              <div class="College_cell">
+                State
+      </div>
+              <div class="College_cell">
+                Country
+      </div>
+              
+              <div class="College_cell">
+                Courses
       </div>
             </div>
             
-
           </div>
         </Fade>
       </div>
