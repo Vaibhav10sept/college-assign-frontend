4682f53336fcd046b346a3023662d59c5072fe2c
Vaibhav10sept <vaibhavtiwari10sept@gmail.com>
2021-04-29 18:34:36 +0530

    vfbvfdsdb

--- src/Components/Courses/Course.js
+++ src/Components/Courses/Course.js
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from "react";
 import '../Courses/courses.css'
 import Fade from 'react-reveal/Fade';
 import axios from "axios";
-import { Scale } from "styled-loaders-react";
-
 import { useHistory } from "react-router-dom";
 import CubeGrid from "styled-loaders-react/lib/components/CubeGrid";
+
 function Course({course}) {
   const [fetcheddata, setfetcheddata] = useState();
   const history = useHistory();
-  React.useEffect(() => {
+  useEffect(() => {
   axios
       .get(`https://college-backend-assignment.herokuapp.com/api/college/course/${course}`)
       .then((res) => {
